18d4d4d9a391311e32ed34f238b75dcb1f527210
Paul Kim <paul.eun.kim@gmail.com>
2023-06-06 15:09:21 -0600

    got a basic card working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,109 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import becks from "./becks.jpg";
 
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
-import Typography from '@mui/material/Typography';
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 
+// icons
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import LaptopMacIcon from "@mui/icons-material/LaptopMac";
+import HotelIcon from "@mui/icons-material/Hotel";
+import LinkIcon from "@mui/icons-material/Link";
+
+import RepeatIcon from "@mui/icons-material/Repeat";
+import Typography from "@mui/material/Typography";
+import { Avatar } from "@mui/material";
+
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+
+//lists
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
+import data from "./resumeContents";
+
+const experience = [<React.Fragment>1</React.Fragment>,<React.Fragment>2</React.Fragment>];
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 22 }} color="text.primary" gutterBottom>
+        {data.experience.title[0]}
+      </Typography>
+
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        Jan 2023 - May 2023
+        {experience[0]}
+      </Typography>
+
+      <Typography variant="body2">
+            {data.experience.description[0]}
+      </Typography>
+    </CardContent>
+
+    <CardActions>
+      <Button size="small">
+        <LinkIcon></LinkIcon>
+      </Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+function OutlinedCard() {
+  return (
+    <Box sx={{ minWidth: 275, maxWidth: 500 }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
+}
 function CustomizedTimeline() {
   return (
-    <Timeline position="alternate">
+    <Timeline position="right">
+      {/* Becks */}
       <TimelineItem>
         <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
+          sx={{ m: "auto 0" }}
           align="right"
           variant="body2"
           color="text.secondary"
         >
-          9:30 am
+          Spring 2023
         </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot>
-            <FastfoodIcon />
+            <Avatar alt="Remy Sharp" src={becks} sx={{ width: 128, height: 128 }}/>
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Eat
+        <TimelineContent sx={{ py: "12px", px: 2 }}>
+          {/* <Typography variant="h6" component="span">
+            Machine Learning Research Intern
           </Typography>
-          <Typography>Because you need strength</Typography>
+          <Typography>Because you need strength</Typography> */}
+          <OutlinedCard></OutlinedCard>
         </TimelineContent>
       </TimelineItem>
+
+      {/* Mathematics Research */}
       <TimelineItem>
         <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
+          sx={{ m: "auto 0" }}
           variant="body2"
           color="text.secondary"
         >
-          10:00 am
+          Spring 2023
         </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
@@ -55,41 +112,53 @@ function CustomizedTimeline() {
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
+        <TimelineContent sx={{ py: "12px", px: 2 }}>
           <Typography variant="h6" component="span">
             Code
           </Typography>
           <Typography>Because it&apos;s awesome!</Typography>
         </TimelineContent>
       </TimelineItem>
+
       <TimelineItem>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot color="primary" variant="outlined">
             <HotelIcon />
           </TimelineDot>
-          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
         </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
+        <TimelineContent sx={{ py: "12px", px: 2 }}>
           <Typography variant="h6" component="span">
             Sleep
           </Typography>
           <Typography>Because you need rest</Typography>
         </TimelineContent>
       </TimelineItem>
+
       <TimelineItem>
         <TimelineSeparator>
-          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
           <TimelineDot color="secondary">
             <RepeatIcon />
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
+        <TimelineContent sx={{ py: "12px", px: 2 }}>
           <Typography variant="h6" component="span">
             Repeat
           </Typography>
           <Typography>Because this is the life you love!</Typography>
+        </TimelineContent>
+      </TimelineItem>
+
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+        </TimelineSeparator>
+        <TimelineContent>
+          What is this do
+          <Typography>What happens here</Typography>
         </TimelineContent>
       </TimelineItem>
     </Timeline>
@@ -98,10 +167,12 @@ function CustomizedTimeline() {
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <CustomizedTimeline></CustomizedTimeline>
-  )
-}
+    <div>
+      <CustomizedTimeline></CustomizedTimeline>
+    </div>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
---
+++ src/pages/resumeContents.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+
+//list
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
+const resume = {
+    "experience": {
+
+      "title": [
+        "Machine Learning Engineer Intern",
+        "Mathematics Research Intern",
+        "Warehouse Associate",
+        "Software Development Intern"
+      ],
+
+      "company": [
+        "Beck's Hybrids",
+        "Arizona State University",
+        "Amazon",
+        "City of Aurora"
+      ],
+
+      "tenure": [
+        "Jan 2023 - May 2023",
+        "May 2022 - Aug 2022",
+        "Sep 2018 - May 2022",
+        "Sep 2019 - Mar 2020"
+      ],
+
+      "description": [
+        [
+          <List sx={{ listStyleType: "disc", pl: 4 }}>
+          <ListItem sx={{ display: "list-item" }}>
+          Produced a successful and replicable object detection workflow using convolutional neural networks in ArcGIS for counting corn plants from drone images; setting a pathway to automate the process that was previously done by hand and presented findings to Beck’s Hybrid’s team and the National Data Mine Network
+          </ListItem>
+          <ListItem sx={{ display: "list-item" }}>asdf</ListItem>
+        </List>
+
+        ],
+
+        [
+          "Integrated a variety of linear algebra, numerical analysis, and calculus techniques to de-blur blurry/noisy imagesIntegrated a variety of linear algebra, numerical analysis, and calculus techniques to de-blur blurry/noisy images"
+        ],
+
+        [
+          "Awarded employee of the quarter (Winter ‘18) for working efficiently, accepting the most difficult tasks, and without absences",
+          "Designed a stow cart that was later used as standard practice across other larger Delivery Station facilities"
+        ],
+
+        [
+          "Digitized court operations, processes, and forms using HTML, CSS, JavaScript, and PHP"
+        ]
+      ]
+    }
+  }
+
+
+export default resume
